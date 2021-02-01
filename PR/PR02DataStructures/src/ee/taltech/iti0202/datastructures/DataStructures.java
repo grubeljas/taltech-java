2932ce32ab41c14a2218cd13257c1311f021aae0
pagrub <pagrub@ttu.ee>
2021-02-01 23:32:33 +0200

    ELEVEN

---
+++ PR/PR02DataStructures/src/ee/taltech/iti0202/datastructures/DataStructures.java
@@ -0,0 +1,136 @@
+package ee.taltech.iti0202.datastructures;
+
+import java.util.*;
+
+public class DataStructures {
+
+    Map<String, Integer> student_map = new HashMap<>();
+
+    /**
+     * Given String is a sentence with some words.
+     * There are only single whitespace between every word and no punctuation marks.
+     * Also there are no capital letters in input string.
+     * <p>
+     * Return the longest word from the input sentence.
+     * <p>
+     * If there are more than one word with the same length then return the word which comes alphabetically first.
+     * <p>
+     * Hints:
+     * You can split words into an array using "str.split()"
+     * Sorting the list with the longest words can definitely help you to find the word which comes alphabetically first.
+     *
+     * @param sentence input String to find the longest words
+     * @return the longest String from input
+     */
+    public static String findLongestWord(String sentence) {
+        String longest = "";
+        int longes = 0;
+        String[] array = sentence.split(" ");
+        Arrays.sort(array);
+        for (String el: array) {
+            if (el.length() > longes) {
+                longes = el.length();
+                longest = el;
+            }
+        }
+        return longest;
+    }
+
+    /**
+     * Classic count the words exercise.
+     * <p>
+     * From input count all the words and collect results to map.
+     *
+     * @param sentence array of strings, can't be null.
+     * @return map containing all word to count mappings.
+     */
+    public static Map<String, Integer> wordCount(String[] sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String name : sentence) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        return map;
+    }
+
+    /**
+     * Loop over the given list of strings to build a resulting list of string like this:
+     * when a string appears the 2nd, 4th, 6th, etc. time in the list, append the string to the result.
+     * <p>
+     * Return the empty list if no string appears a 2nd time.
+     * <p>
+     * Use map to count times that the string has appeared.
+     *
+     * @param words input list to filter
+     * @return list of strings matching criteria
+     */
+    public static List<String> onlyEvenWords(List<String> words) {
+        String[] myArray = new String[words.size()];
+        List<String> even = new LinkedList<>();
+        words.toArray(myArray);
+        Map<String, Integer> map = wordCount(myArray);
+        for (String i: map.keySet()) {
+            if (map.get(i) % 2 == 0) {
+                even.add(i);
+            }
+        }
+        return even;
+    }
+
+    /**
+     * Method to save student and student's grade(you should use a Map here).
+     * Only add student if his/hers grade is in the range of 0-5.
+     *
+     * @param studentInfo String with a pattern (name:grade)
+     */
+    public void addStudent(String studentInfo) {
+        String[] array = studentInfo.split(":");
+        int n = Integer.parseInt(array[1]);
+        if (0 <= n && n <= 5){
+            student_map.put(array[0], n);
+        } else {
+            student_map.put(array[0], -1);
+        }
+    }
+
+    /**
+     * Method to get student's grade.
+     * Return the student's grade by his/hers name.
+     * You can assume that the user is already added(previous function with student's info already called).
+     *
+     * @param name String students name
+     * @return int student's grade.
+     */
+    public int getStudentGrade(String name) {
+        return student_map.get(name);
+    }
+
+    /**
+     * Main.
+     * @param args Commend line arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println(findLongestWord("nimi on salastatud"));  // "salastatud"
+        System.out.println(findLongestWord("aaa bbbbb"));  // "bbbbb"
+        System.out.println(findLongestWord("hello ahllo")); // "ahllo"
+
+        System.out.println(wordCount(new String[]{})); // empty
+        System.out.println(wordCount(new String[]{"eggs", "SPAM", "eggs", "bacon", "SPAM", "bacon", "SPAM"})); // {bacon=2, eggs=2, SPAM=3}
+
+        System.out.println(onlyEvenWords(Arrays.asList("foo", "bar", "baz", "baz", "bar", "foo"))); // [baz, bar, foo]
+        System.out.println(onlyEvenWords(Arrays.asList("a", "b", "b", "a"))); // [b, a]
+        System.out.println(onlyEvenWords(Arrays.asList("eggs", "bacon", "SPAM", "ham", "SPAM"))); // [SPAM]
+
+        DataStructures dataStructures = new DataStructures();
+
+        dataStructures.addStudent("Ago:5");
+        dataStructures.addStudent("Martin:0");
+        dataStructures.addStudent("Margo:3");
+        dataStructures.addStudent("Cheater:6");
+
+        System.out.println(dataStructures.getStudentGrade("Ago")); // 5
+        System.out.println(dataStructures.getStudentGrade("Martin")); // 0
+        System.out.println(dataStructures.getStudentGrade("Margo")); // 3
+        System.out.println(dataStructures.getStudentGrade("Cheater")); // -1
+    }
+}
+
