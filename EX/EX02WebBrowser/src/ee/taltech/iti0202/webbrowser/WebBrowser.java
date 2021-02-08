ee835a45e33949fc02e8e85d3dea47aec0768ca4
pagrub <pagrub@ttu.ee>
2021-02-08 18:17:36 +0200

    unused import

--- EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowser.java
+++ EX/EX02WebBrowser/src/ee/taltech/iti0202/webbrowser/WebBrowser.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
 
