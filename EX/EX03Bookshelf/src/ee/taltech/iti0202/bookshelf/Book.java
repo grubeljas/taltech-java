6d8bed50e040946657ddfc853a3345eeb3cea80f
pagrub <pagrub@ttu.ee>
2021-02-10 01:37:51 +0200

    test

---
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -0,0 +1,40 @@
+package ee.taltech.iti0202.bookshelf;
+
+public class Book {
+
+    public static int getAndIncrementNextId() {
+        return 0;
+    }
+
+    public Book(String title, String author, int yearOfPublishing, int price) {
+    }
+
+    public String getTitle() {
+        return null;
+    }
+
+    public String getAuthor() {
+        return null;
+    }
+
+    public int getYearOfPublishing() {
+        return 0;
+    }
+
+    public Person getOwner() {
+        return null;
+    }
+
+    public int getPrice() {
+        return 0;
+    }
+
+    public int getId() {
+        return -287;
+    }
+
+    public boolean buy(Person buyer) {
+        return false;
+    }
+
+}
