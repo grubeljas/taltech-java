3419748c9e794b39e6e08f4760d2d00ca1dd7611
pagrub <pagrub@ttu.ee>
2021-02-10 02:54:01 +0200

    book buy

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Person.java
@@ -1,28 +1,55 @@
 package ee.taltech.iti0202.bookshelf;
 
-import java.util.TreeMap;
+//import java.util.TreeMap;
 
 public class Person {
 
     private String name; private int money;
 
+    /**
+     * Constructor.
+     *
+     * @param name of human.
+     * @param money right now.
+     */
     public Person(String name, int money) {
         this.name = name;
         this.money = money;
     }
 
+    /**
+     * Get money.
+     *
+     * @return amount of money.
+     */
     public int getMoney() {
         return money;
     }
 
+    /**
+     * Set money.
+     *
+     * @param money new amount.
+     */
     public void setMoney(int money) {
         this.money = money;
     }
 
+    /**
+     * Get name.
+     *
+     * @return name of person.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Buy book.
+     *
+     * @param book of deal.
+     * @return bool.
+     */
     public boolean buyBook(Book book) {
         if (book == null) {
             return false;
@@ -34,6 +61,12 @@ public class Person {
         return false;
     }
 
+    /**
+     * Sell book.
+     *
+     * @param book of deal and sell.
+     * @return bool of success
+     */
     public boolean sellBook(Book book) {
         if (book == null) {
             return false;
