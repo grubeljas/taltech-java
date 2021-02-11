00613f2da134841ca812115551cf845f00284457
pagrub <pagrub@ttu.ee>
2021-02-11 03:37:45 +0200

    second part

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -1,5 +1,9 @@
 package ee.taltech.iti0202.bookshelf;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Book {
 
     private String title;
@@ -7,7 +11,10 @@ public class Book {
     private int year, price;
     static int id = -1;
     int number;
-    private Person owner;
+    private Person owner = null;
+    static List<Book> data = new ArrayList<>();
+    static String previousAuthor;
+    static int prevYear;
 
     /**
      * Make id.
@@ -32,6 +39,95 @@ public class Book {
         this.price = price;
         this.year = yearOfPublishing;
         this.number = getAndIncrementNextId();
+    }
+
+    /**
+     * Create book.
+     *
+     * @param title of book
+     * @param author yeah
+     * @param yearOfPublishing amazing
+     * @param price omg
+     * @return masterpiece
+     */
+    public static Book of(String title, String author, int yearOfPublishing, int price) {
+        for (Book el: data) {
+            if (el.title.equals(title) && el.author.equals(author) && el.year == yearOfPublishing) {
+                return el;
+            }
+        }
+        previousAuthor = author;
+        prevYear = yearOfPublishing;
+        Book book = new Book(title, author, yearOfPublishing, price);
+        data.add(book);
+        return book;
+    }
+
+    /**
+     * Create book with same author like aa previous one.
+     *
+     * @param title new
+     * @param price chipper i hope
+     * @return boook
+     */
+    public static Book of(String title, int price) {
+        if (data.isEmpty()) {
+            return null;
+        } else {
+            return of(title, previousAuthor, prevYear, price);
+        }
+    }
+
+    /**
+     * All books of that person.
+     *
+     * @param owner not me
+     * @return collection.
+     */
+    public static List<Book> getBooksByOwner(Person owner) {
+        List<Book> collection = new LinkedList<>();
+        for (Book book: data) {
+            if (book.getOwner() != null) {
+                if (book.getOwner().getName().equals(owner.getName())) {
+                    collection.add(book);
+                }
+            }
+        }
+        return collection;
+    }
+
+    /**
+     * Remove book from list.
+     *
+     * @param book of book.
+     * @return true if book exist.
+     */
+    public static boolean removeBook(Book book) {
+        if (book == null) {
+            return false;
+        }
+        if(data.contains(book)) {
+            book.buy(null);
+            data.remove(book);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get all harry potter books.
+     *
+     * @param author of .
+     * @return alllll.
+     */
+    public static List<Book> getBooksByAuthor(String author) {
+        List<Book> collection = new LinkedList<>();
+        for (Book book: data) {
+            if (book.getAuthor().toLowerCase().equals(author.toLowerCase())) {
+                collection.add(book);
+            }
+        }
+        return collection;
     }
 
     /**
