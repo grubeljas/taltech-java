b2285b24cbe81dbb0bc2065d4e4f0e8d9a0666d4
pagrub <pagrub@ttu.ee>
2021-02-15 22:20:38 +0200

    second part

---
+++ PR/PR04Stock/src/ee/taltech/iti0202/stock/product/Product.java
@@ -0,0 +1,66 @@
+package ee.taltech.iti0202.stock.product;
+import ee.taltech.iti0202.stock.exceptions.StockException;
+
+public class Product {
+
+    /**
+     * Create a new product with the given name and price.
+     * <p>
+     * If the price is negative, throw a StockException with a NEGATIVE_PRICE reason.
+     *
+     * @param name Name of the product.
+     * @param price Price of the product.
+     * @throws StockException NEGATIVE_PRICE
+     */
+    public Product(String name, int price) throws StockException {
+        if (price < 0) {
+            throw new StockException(StockException.Reason.NEGATIVE_PRICE);
+        } else {
+            this.price = price;
+            this.name = name;
+            this.idOfProduct = getNextId();
+        }
+    }
+
+    private String name;
+    private int price, idOfProduct;
+    static int id = 1;
+
+    /**
+     * Returns the next id.
+     *
+     * This value has to increment with every call.
+     *
+     * @return The next id.
+     */
+    public static int getNextId() {
+        return id++;
+    }
+
+    /**
+     * Returns id of the product.
+     *
+     * @return id of the product.
+     */
+    public int getId() {
+        return idOfProduct;
+    }
+
+    /**
+     * Returns the name of the product.
+     *
+     * @return the name of the product.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the price of the product.
+     *
+     * @return the price of the product.
+     */
+    public int getPrice() {
+        return price;
+    }
+}
