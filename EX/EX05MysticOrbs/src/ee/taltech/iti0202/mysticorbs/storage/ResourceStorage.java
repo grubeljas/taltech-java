159de956d6afc9021148e9144f351daefba4838b
pagrub <pagrub@ttu.ee>
2021-02-23 18:37:12 +0200

    EX05

---
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/storage/ResourceStorage.java
@@ -0,0 +1,50 @@
+package ee.taltech.iti0202.mysticorbs.storage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceStorage {
+
+    public Map<String, Integer> resources;
+
+    public ResourceStorage() {
+        resources = new HashMap<>();
+    }
+
+    public boolean isEmpty() {
+        for (String resource: resources.keySet()) {
+            if (resources.get(resource) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addResource(String resource, int amount) {
+        if (resources.containsKey(resource.toLowerCase()) && amount > 0) {
+            resources.put(resource.toLowerCase(), resources.get(resource.toLowerCase()) + amount);
+        } else if (amount > 0) {
+            resources.put(resource.toLowerCase(), amount);
+        }
+    }
+
+    public int getResourceAmount(String resource) {
+        if (resources.containsKey(resource.toLowerCase()) && resources.get(resource.toLowerCase()) > 0) {
+            return resources.get(resource.toLowerCase());
+        }
+        return 0;
+    }
+
+    public boolean hasEnoughResource(String resource, int amount) {
+        return amount > 0 && getResourceAmount(resource) >= amount;
+    }
+
+    public boolean takeResource(String resource, int amount) {
+        if (hasEnoughResource(resource, amount)) {
+            resources.put(resource.toLowerCase(),resources.get(resource.toLowerCase()) - amount);
+            return true;
+        }
+        return false;
+    }
+
+}
