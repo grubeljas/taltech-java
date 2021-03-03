13d0a02077c7a74d79243024a1066cf7794684e8
pagrub <pagrub@ttu.ee>
2021-03-04 01:42:34 +0200

    uniwue animals

--- PR/PR06Shelter/src/ee/taltech/iti0202/shelter/animalprovider/TestAnimalProvider.java
+++ PR/PR06Shelter/src/ee/taltech/iti0202/shelter/animalprovider/TestAnimalProvider.java
@@ -4,38 +4,54 @@ import ee.taltech.iti0202.shelter.animal.Hirola;
 import ee.taltech.iti0202.shelter.animal.Tarantula;
 import ee.taltech.iti0202.shelter.animal.Wombat;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TestAnimalProvider implements AnimalProvider {
 
-    public String color;
+    public Set<Animal> animals;
+    public int i;
 
     /**
      * Constructor.
      */
     public TestAnimalProvider() {
-        this.color = "red";
+        i = 0;
+        this.animals = new HashSet<>();
+        animals.add(new Hirola("red"));
+        animals.add(new Hirola("red"));
+        animals.add(new Hirola("white"));
+        animals.add(new Tarantula("red"));
+        animals.add(new Tarantula("green"));
+        animals.add(new Tarantula("green"));
+        animals.add(new Tarantula("red"));
+        animals.add(new Wombat("red"));
+        animals.add(new Wombat("green"));
+        animals.add(new Wombat("green"));
+        animals.add(new Wombat("red"));
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
 
     @Override
     public List<Animal> provide(Animal.Type type) {
-        List<Animal> animals = new LinkedList<>();
-        for (short i = 0; i < 5; i++) {
-            Animal animal;
-            if (type.equals(Animal.Type.HIROLA)) {
-                animal = new Hirola(color);
-            } else if (type.equals(Animal.Type.TARANTULA)) {
-                animal = new Tarantula(color);
-            } else {
-                animal = new Wombat(color);
+        Set<Animal> providedAnimalsSet = animals.stream()
+                .filter(animal1 -> animal1.type.equals(type))
+                .collect(Collectors.toSet());
+        List<Animal> providedAnimals = new LinkedList<>(providedAnimalsSet);
+        try {
+            providedAnimals = providedAnimals.subList(i, i + 3);
+            i += 3;
+        } catch (IndexOutOfBoundsException e) {
+            try {
+                providedAnimals = providedAnimals.subList(i, i + 2);
+                i += 2;
+            } catch (IndexOutOfBoundsException e1) {
+                providedAnimals.clear();
             }
-            animals.add(animal);
         }
-        return animals;
+        return providedAnimals;
     }
 }
