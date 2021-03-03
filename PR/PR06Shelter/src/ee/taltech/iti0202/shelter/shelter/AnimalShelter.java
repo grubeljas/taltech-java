bf4bc4c0e83e952d39504ecb0ee5b27521506d89
pagrub <pagrub@ttu.ee>
2021-03-04 01:19:26 +0200

    style

--- PR/PR06Shelter/src/ee/taltech/iti0202/shelter/shelter/AnimalShelter.java
+++ PR/PR06Shelter/src/ee/taltech/iti0202/shelter/shelter/AnimalShelter.java
@@ -38,7 +38,6 @@ public class AnimalShelter {
      */
     public List<Animal> getAnimals(Animal.Type animalType, String color, int count) {
         List<Animal> neededAnimals = new LinkedList<>();
-        provider.provide(animalType);
         while (neededAnimals.size() < count) {
             List<Animal> fromProvider = provider.provide(animalType);
             if (fromProvider.isEmpty()) {
